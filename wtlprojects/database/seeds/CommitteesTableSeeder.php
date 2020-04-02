3295eb8cb248aac85b19a193befdf1b8904feff2
Rohit Pai <rohit23110@gmail.com>
2020-04-02 18:34:47 +0530

    Resolve Conflict

--- wtlprojects/database/seeds/CommitteesTableSeeder.php
+++ wtlprojects/database/seeds/CommitteesTableSeeder.php
@@ -19,7 +19,7 @@ class CommitteesTableSeeder extends Seeder
         for ($i=0; $i < 5; $i++) {
             Committee::create([
                 'name' => $faker->name,
-                'email' => $faker->email,
+                'login_id' => $faker->bothify('?#??##'),
                 'password' => $password
             ]);
         }
